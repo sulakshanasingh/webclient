c6ef193a868c9184f2c61a0ec16ffe4a3ecf7314
sulakshana.singh <r.sulakshana@yahoo.com>
2024-11-09 14:05:23 -0600

    ssl code

--- src/main/java/com/wc/config/WebclientConfig.java
+++ src/main/java/com/wc/config/WebclientConfig.java
@@ -1,5 +1,7 @@
 package com.wc.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
@@ -17,13 +19,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
 import org.springframework.web.reactive.function.client.*;
 import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.resources.ConnectionProvider;
 import reactor.util.retry.Retry;
 
+import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLException;
+import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
@@ -32,14 +43,14 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class WebclientConfig {
     @Bean
-    public WebClient webclient() throws SSLException {
-        //ssl configuration
-        SslContext sslContext = SslContextBuilder
-                .forClient()
-                //.keyManager()
-                //.trustManager()
-                .trustManager(InsecureTrustManagerFactory.INSTANCE)// disable SSL check for test code
-                .build();
+    public ObjectMapper getObjectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd"));
+        return mapper;
+    }
+    @Bean
+    public WebClient webclient() throws Exception {
         //Connection pooling configuration
         ConnectionProvider connectionProvider = ConnectionProvider.builder("myConnectionPool")
                 .maxConnections(500)
@@ -54,7 +65,13 @@ public class WebclientConfig {
                 .compress(true)
                 //enable logging of Http request and response
                 .wiretap(true)
-                .secure(sslContextSpec -> sslContextSpec.sslContext(sslContext))
+                .secure(sslContextSpec -> {
+                    try {
+                        sslContextSpec.sslContext(sslContext());
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                })
                 .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
                 .responseTimeout(Duration.ofMillis(5000))
                 .doOnConnected(conn ->
@@ -77,9 +94,31 @@ public class WebclientConfig {
                                 .doAfterRetry(retrySignal -> log.info("Retrying ..."))))
                 //add circuit breaker
                 .filter((request, next) -> circuitBreaker.executeSupplier(() -> next.exchange(request)))
+                .codecs(configurer -> configurer.defaultCodecs().jackson2JsonDecoder(new Jackson2JsonDecoder(getObjectMapper())))
                 .build();
 
         return client;
+    }
+    private SslContext sslContext() throws KeyStoreException,
+            CertificateException, IOException,
+            NoSuchAlgorithmException,
+            UnrecoverableKeyException {
+        //disable ssl validation
+        SslContext sslContext = SslContextBuilder
+                .forClient()
+                .trustManager(InsecureTrustManagerFactory.INSTANCE)
+                .build();
+        //enable ssl validation
+        KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+        keyStore.load(this.getClass().getClassLoader()
+                .getResourceAsStream("path"), "password".toCharArray());
+
+        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
+        keyManagerFactory.init(keyStore, "password".toCharArray());
+
+        return SslContextBuilder.forClient()
+                .keyManager(keyManagerFactory)
+                .build();
     }
     private ExchangeFilterFunction logRequest() {
         return ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {
